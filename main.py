a1ef1c2671891bc0f716419443c33ca47918152a
Lailatul Qadri Zakaria <61860775+lailatulqadri@users.noreply.github.com>
2023-06-08 14:05:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
-#list - important text features for depression identification:
-# 1) absolute words - source: https://speakai.co/absolutist-words/
-absolutist_word =['absolutely','all','always','complete','completely','constant','constantly','definitely','entire','ever','every','everyone','everything','full','must','never','nothing','totally','whole']
-# 2) first person singular & plurl (sometimes we use plural to refer to ourselve)
-first_person_singular = ['me','myself', 'i', 'mine', 'my', 'ourself']
-first_person_plural = ['we', 'us', 'our', 'ours', 'ourselves']
-
 st.title("Extracting Depression Linguistic Features by using Natural Language Processing")
